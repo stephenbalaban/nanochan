193ea093b61ed0e1410ee182cf4382607f07b883
Simon McFarlane <simon@piston.pw>
2014-10-28 23:22:54 +0000

    Remove functionality from nano.js

--- nano.js
+++ nano.js
@@ -2,10 +2,6 @@
 var config = require('./config')
 
 var irc = require("irc");
-var youtube = require("youtube-node");
-var https = require("https");
-var sys = require("sys");
-var redis = require("redis");
 
 var modules = [];
 
@@ -24,19 +20,6 @@ modules.forEach(function (module) {
   module.register(bot);
 });
 
-tells = {};
-
-var stdin = process.openStdin();
-
-stdin.addListener("data", function(d) {
-  // note:  d is an object, and when converted to a string it will
-  // end with a linefeed.  so we (rather crudely) account for that  
-  // with toString() and then substring() 
-    bot.say("#lainchan", d.toString().substring(0, d.length-1));
-});
-
-youtube.setKey('AIzaSyB70sBWjcC5oG6dZ9AsJcDvUQVDS4bKlQw');
-
 bot.addListener('message', function (from, to, message) {
 
   if(from != "nanochan") { // the bot can never reply to itself
@@ -44,20 +27,7 @@ bot.addListener('message', function (from, to, message) {
     if(message.indexOf(config.botName) > -1){
       //bot.say(to, "Were you talking about me " + from + "?");
     }
-    if (message[0] == '.') {
-      command = message.slice(1);
-      switch (command) {
-        case 'beer':
-          bot.say(to, "One beer coming up!");
-          break;
-      }
-    } else if (message.indexOf("?v=") > -1) {
-      url = message.split(/\W+/);
-      url = url[url.indexOf('v') + 1];
-      youtube.getById(url, function(resultData) {
-        bot.say(to, "Video: " + resultData.items[0].snippet.title);
-      });
-    } else if (message.toLowerCase() == "hi" || message.toLowerCase() == "hey" || message.toLowerCase() == "hello"){
+  if (message.toLowerCase() == "hi" || message.toLowerCase() == "hey" || message.toLowerCase() == "hello"){
       bot.say(to, "Hi " + from + "!");
     }
   }
