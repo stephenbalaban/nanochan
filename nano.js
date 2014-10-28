4aac25479136ab2e08c76a270c32ead2b327d10c
Simon McFarlane <simon@piston.pw>
2014-10-28 23:19:53 +0000

    Move .tells to module

--- nano.js
+++ nano.js
@@ -37,24 +37,7 @@ stdin.addListener("data", function(d) {
 
 youtube.setKey('AIzaSyB70sBWjcC5oG6dZ9AsJcDvUQVDS4bKlQw');
 
-
-lainchan_regex = /(lainchan\.org\/[\w|%]+\/res\/\d+)/;  
-
-var client = redis.createClient();
-client.get('tells', function(e,r) {
-  tells = JSON.parse(r);
-});
-
 bot.addListener('message', function (from, to, message) {
-  name = from.toLowerCase();
-  if(name in tells) {
-    bot.say(to, "Hey " + from + ", I have messages for you!");
-    tells[name].forEach(function(element, index, array) {
-      bot.say(from, element[0] + " says \"" + element[1] + "\"");
-    });
-    delete tells[name];
-    client.set('tells', JSON.stringify(tells));
-  }
 
   if(from != "nanochan") { // the bot can never reply to itself
     console.log(from + ' => ' + to + ': ' + message);
@@ -62,22 +45,11 @@ bot.addListener('message', function (from, to, message) {
       //bot.say(to, "Were you talking about me " + from + "?");
     }
     if (message[0] == '.') {
-      tell = message.split(' ');
-      if(tell[0] == '.tell' && tell.length >= 3){
-        name = tell[1].toLowerCase();
-        if (!(name in tells)){
-          tells[name] = [];
-        }
-        tells[name].push([from, tell.slice(2).join(' ')]);
-        bot.say(to, "Alright " + from + " I'll tell " + name + " next time I see them!");
-        client.set('tells', JSON.stringify(tells));
-      } else {
-        command = message.slice(1);
-        switch (command) {
-          case 'beer':
-            bot.say(to, "One beer coming up!");
-            break;
-        }
+      command = message.slice(1);
+      switch (command) {
+        case 'beer':
+          bot.say(to, "One beer coming up!");
+          break;
       }
     } else if (message.indexOf("?v=") > -1) {
       url = message.split(/\W+/);
