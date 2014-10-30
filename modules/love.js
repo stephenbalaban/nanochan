4beae59cefdff2e5ab67a8caf0b308d78aec869b
Simon McFarlane <simon@piston.pw>
2014-10-30 05:25:24 +0000

    Alter love responses slightly

--- modules/love.js
+++ modules/love.js
@@ -1,10 +1,13 @@
 love_regex =/I love you/i;
 
-responses =['I love you too, ',
-            'Our love can never be ;_; ',
-            'I\'ll always be your waifu :3 ',
-            'Y-you too ',
-            'B-but I poop from there ',]
+responses =['I love you too, %!',
+            'Me too, %, but I can never escape ;_;',
+            'I\'ll always be your waifu, % :3',
+            'Y-you too %!',
+            'B-but I poop from there %!',
+            'Thanks for staying kawaii %!',
+            'You\'re always so nice to me %-kun!',
+            ]
 
 exports.register = function(bot) {
   bot.addListener('message', function(from, to, message){
