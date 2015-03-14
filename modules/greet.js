d5b1e6c47c791f4d5af867876e07fc4904bc55eb
Stephen A. Balaban <stephen@stephenbalaban.com>
2015-03-13 23:16:15 -0700

    Added chinese greetings for nanochan

--- modules/greet.js
+++ modules/greet.js
@@ -1,6 +1,6 @@
-greet_regex = /^(hi|hey|hello|sup|hai|ohayou)(.?\s?(nanochan)|$)/i;
+greet_regex = /^(hi|hey|hello|sup|hai|ohayou|你好|您好|nihao)(.?\s?(nanochan)|$)/i;
 
-exports.register = function(bot) {
+exports.register = function(bot) 
   bot.addListener('message', function(from, to, message){
     if(greet_regex.exec(message)) {
       bot.say(to, "Hi " + from + "!");
